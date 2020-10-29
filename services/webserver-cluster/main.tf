c1ced0db346c4cd64a65720b34ea2250a971dff8
ivanmbizhev <73398594+ivanmbizhev@users.noreply.github.com>
2020-10-29 20:02:36 +0200

    Initial commit of modules repo

---
+++ services/webserver-cluster/main.tf
@@ -0,0 +1,97 @@
+
+terraform {
+    required_version = ">= 0.12.26"
+}
+
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = var.min_size
+  max_size = var.max_size
+
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = var.cluster_name
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  
+  image_id        = "ami-09ba01d9b2eb740ba"
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "${var.cluster_name}-instance"
+
+  
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_elb" "example" {
+  name               = var.cluster_name
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  listener {
+    lb_port           = var.elb_port
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+
+resource "aws_security_group" "elb" {
+  name = "${var.cluster_name}-elb"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+  ingress {
+    from_port   = var.elb_port
+    to_port     = var.elb_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
